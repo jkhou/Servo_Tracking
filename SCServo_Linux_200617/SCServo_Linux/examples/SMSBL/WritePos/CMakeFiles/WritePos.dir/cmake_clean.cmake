a10dc5f3647aeb57dbaae8d7a322e8a40591eca2
LiJiayi96 <jiayili96@gmail.com>
2020-11-20 21:19:36 +0800

    add servo specification

---
+++ SCServo_Linux_200617/SCServo_Linux/examples/SMSBL/WritePos/CMakeFiles/WritePos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WritePos.dir/WritePos.cpp.o"
+  "WritePos.pdb"
+  "WritePos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WritePos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
